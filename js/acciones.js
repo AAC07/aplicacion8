8a41b4b670305bfb9b8752ab26b8fdfeac664d7a
AAC07 <alejandrodsaupoo.3f@gmail.com>
2016-05-12 13:39:51 -0500

    2subida

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,7 @@
 $(document).ready(function(e) {
     document.addEventListener('deviceready', function() {
 		$('#btnbeep').tap(function (){
-			navigator.notification.beep(1);//numero de veces que deseas que notifique
+			navigator.notification.beep(3);//numero de veces que deseas que notifique
 		}); //beep
 		$('#btnvibrar').on('tap', function () {
 			navigator.notification.vibrate(4000);
